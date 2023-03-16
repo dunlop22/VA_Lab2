066e3c40f4e2c239a4f85a1ab1a516e2b8b4263c
Пользователь <20020110ll@gmail.com>
2023-03-17 01:22:03 +0700

    ready (without generation)

--- VA_Lab2/VA_Lab2.cpp
+++ VA_Lab2/VA_Lab2.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 int num_str = 0;    //количество строк в файле
 
-double** Mas1;  //динамический массив под исходную матрицу
-double* Mas2;   //динамический массив под b
-double** Mas3;  //динамический массив под итоговую матрицу
-double* Mas4;   //динамический массив под решение (X)
-double* Mas5;   //динамический массив под невязки
+double** Mas1;  //динамический массив под исходную матрицу (n*n)
+double* Mas2;   //динамический массив под b (1*n)
+double** Mas3;  //динамический массив для размещения 1 и 2 исходных матриц
+double* Mas4_1;   //динамический массив под решение (X)
+double* Mas4_2;   //динамический массив под решение (X)
+double* Mas5_1;   //динамический массив под невязки Якоби
+double* Mas5_2;   //динамический массив под невязки Зейделя
 
-double e;       //значение точности
+double e = 0 ;       //значение точности
 
 //генерация случайных значений матрицы
 void generate_matrx()
@@ -20,7 +22,8 @@ void generate_matrx()
     {
         for (int j = 0; j < num_str; j++)
         {
-            Mas1[i][j] = rand() % (25 + 1);     //генерация чисел в заданном промежутке
+            //Mas1[i][j] = rand() % (25 + 1);     //генерация чисел в заданном промежутке (0, 25)
+            Mas1[i][j] = -25 + rand() % (50 + 1);     //генерация чисел в заданном промежутке (-25 25)
             Mas3[i][j] = Mas1[i][j];
         }
     }
@@ -28,7 +31,8 @@ void generate_matrx()
     //генерация вектора b
     for (int i = 0; i < num_str; i++)
     {
-        Mas2[i] = rand() % (25 + 1);     //генерация чисел в заданном промежутке
+        Mas2[i] = -25 + rand() % (50 + 1);     //генерация чисел в заданном промежутке (-25 25)
+        //Mas2[i] = rand() % (25 + 1);     //генерация чисел в заданном промежутке
         Mas3[i][num_str] = Mas2[i];
     }
 }
@@ -142,6 +146,13 @@ void print_matrx(double** Mas, int func)
     }
 }
 
+void print_residuals(double* Mas5)
+{
+    for (int i=0;i<num_str;i++)
+    {
+        cout << Mas5[i] << "\n";
+    }
+}
 
 bool check_dia()
 {
@@ -165,22 +176,25 @@ void init_matrx()
 
     Mas2 = new double[num_str];        //создание динамического массива под вектор b
 
-    Mas3 = new double* [num_str + 1];       //создание динамического массива для итоговой матрицы
+    Mas3 = new double* [num_str + 1];       //создание динамического массива для размещения 1 и 2 исходных матриц
     for (int i = 0; i < num_str + 1; i++)
     {
         Mas3[i] = new double[num_str];
     }
 
-    Mas4 = new double[num_str];        //создание динамического массива
+    Mas4_1 = new double[num_str];        //создание динамического массива
+    Mas4_2 = new double[num_str];        //создание динамического массива
+    Mas5_1 = new double[num_str];        //создание динамического массива под невязки Якоби
+    Mas5_2 = new double[num_str];        //создание динамического массива под невязки Зейделя
 }
 
-bool check_e(double* Mas4_copy)
+bool check_e(double* Mas4_copy, double* Mas4)
 {
     double num = 0;
     double temp;
     for (int i = 0; i < num_str && num < e; i++)
     {
-        temp = abs(Mas4[i] - Mas4_copy[i]);
+        temp = abs(Mas4_copy[i] - Mas4[i]);
         if (temp > num)     //подсчет максимального значения разности
         {
             num = temp;
@@ -200,7 +214,8 @@ int jacob(int max_num_iter)
     double* Mas4_copy = new double[num_str];      //динамический массив для исходной матрицы b
     for (int i = 0; i < num_str; i++)
     {
-        Mas4[i] = Mas2[i] / Mas3[i][i];
+        Mas4_1[i] = Mas2[i] / Mas3[i][i];
+        Mas4_2[i] = Mas2[i] / Mas3[i][i];
         //cout << Mas4[i] << "  ";
     }
 
@@ -208,7 +223,7 @@ int jacob(int max_num_iter)
     {
         for (int i = 0; i < num_str; i++)
         {
-            Mas4_copy = Mas4;
+            Mas4_copy[i] = Mas4_1[i];
         }
 
         for (int i = 0; i < num_str; i++)
@@ -222,11 +237,15 @@ int jacob(int max_num_iter)
                     
                 }
             }
-            Mas4[i] = (Mas2[i] - summa) / Mas3[i][i];
-            cout << Mas4[i];
+            Mas4_1[i] = (Mas2[i] - summa) / Mas3[i][i];
+            //cout << Mas4[i];
+            if (Mas4_1[i] == INFINITY)
+            {
+                num_iter = max_num_iter - 1;
+            }
         }
         num_iter = num_iter + 1;
-        if (check_e(Mas4_copy))
+        if (check_e(Mas4_copy, Mas4_1))
         {
             break;
         }
@@ -244,14 +263,14 @@ int Zeidel(int max_num_iter)
     double* Mas4_copy = new double[num_str];      //динамический массив для исходной матрицы b
     for (int i = 0; i < num_str; i++)
     {
-        Mas4[i] = Mas2[i] / Mas3[i][i];
+        //Mas4_2[i] = Mas2[i] / Mas3[i][i];
     }
 
     while (num_iter < max_num_iter && num_iter >= 0)
     {
         for (int i = 0; i < num_str; i++)
         {
-            Mas4_copy = Mas4;
+            Mas4_copy[i] = Mas4_2[i];
         }
 
         for (int i = 0; i < num_str; i++)
@@ -262,13 +281,13 @@ int Zeidel(int max_num_iter)
             {
                 if (i != j)
                 {
-                    sum += Mas3[i][j] * Mas4[j];
+                    sum += Mas3[i][j] * Mas4_2[j];
                 }
             }
 
-            Mas4[i] = (Mas3[i][num_str] - sum) / Mas3[i][i];
+            Mas4_2[i] = (Mas3[i][num_str] - sum) / Mas3[i][i];
 
-            if (Mas4[i] != Mas4[i] || Mas4[i] == INFINITY)
+            if (Mas4_2[i] == INFINITY)
             {
                 num_iter = max_num_iter - 1;
                 break;
@@ -276,7 +295,7 @@ int Zeidel(int max_num_iter)
         }
 
         num_iter = num_iter + 1;
-        if (check_e(Mas4_copy))
+        if (check_e(Mas4_copy, Mas4_2))
         {
             break;
         }
@@ -288,6 +307,97 @@ int Zeidel(int max_num_iter)
 
     }
     return num_iter;
+}
+
+//определение невязок
+void residuals(double* Mas4, double* Mas5)
+{
+    for (int i = 0; i < num_str; i++)
+    {
+
+        Mas5[i] = Mas3[i][num_str];
+
+        for (int j = 0; j < num_str; j++)
+        {
+            Mas5[i] = Mas5[i] - Mas3[i][j] * Mas4[j];
+        }
+    }
+}
+
+//определение норм невязок
+void normal_residuals(double* Mas5, double* normal)
+{
+    *normal = fabs(Mas5[0]);
+    for (int i = 0; i < num_str - 1; i++)
+    {
+        if (fabs(Mas5[i + 1]) > *normal)
+        {
+            *normal = fabs(Mas5[i + 1]);
+        }
+    }
+}
+
+int write_good(int num_iter, double* Mas4, double* Mas5)
+{
+    //вывод количества итераций
+    cout << "Количество итераций: " << num_iter;
+
+    //вывод массива 
+    cout << "\n\nМассив X:\n";
+    print_residuals(Mas4);
+
+    //вывод невязок
+    cout << "\n\nНевязки:\n";
+    residuals(Mas4, Mas5);
+    print_residuals(Mas5);
+
+    //вывод нормы невязок
+    double normal_residuals_d;
+    normal_residuals(Mas5_1, &normal_residuals_d);
+    cout << "\n\nНорма невязок: " << normal_residuals_d;
+    
+    //запись в файл
+    std::ofstream out;
+    out.open("file_rez.txt");
+    if (out.is_open())  //файл открыт
+    {
+        out << "Исходная матрица:" << endl;
+        for (int i = 0; i < num_str; i++)
+        {
+            for (int j = 0; j < num_str; j++)
+            {
+                out << Mas3[i][j] << " ";
+            }
+            out << endl;
+        }
+
+        out << endl << "Исходный вектор b:" << endl;
+        for (int i = 0; i < num_str; i++)
+        {
+            out << Mas2[i] << endl;
+        }
+        
+        out << endl << "Вектор X:" << endl;
+        for (int i = 0; i < num_str; i++)
+        {
+            out << Mas4[i] << endl;
+        }
+
+        out << endl << "Невязки" << endl;
+        for (int i = 0; i < num_str; i++) 
+        {
+            out << Mas5[i] << endl;
+        }
+        
+        out << endl << "Норма невязок: " << normal_residuals_d << endl;
+        out.close();        //закрытие файла
+    }
+    else           //при открытии файла произошла ошибка
+    {
+        cout << "\n\nПри записи в файл произошла ошибка!";
+        return 0;
+    }
+    return 1;
 }
 
 int main()
@@ -327,7 +437,7 @@ int main()
             return 0;
         }
     }
-    if (func == 50)     //чтение данных из файла
+    if (func == 50)     //случайная генерация значений
     {
         do
         {
@@ -335,12 +445,13 @@ int main()
             cout << "Введите размерность матрицы: ";
             scanf("%d", &num_str);
         } while (num_str <= 0);
-
+        
+        
         do
         {
             system("cls");      //очистка консоли
             cout << "Введите значение точности: ";
-            scanf("lf", &e);
+            scanf("%lf", &e);
         } while (e > 1);
         
         system("cls");      //очистка консоли
@@ -350,34 +461,55 @@ int main()
         generate_matrx();   //заполнение матрицы случайными значениями
     }
 
+    system("cls");      //очистка консоли
+    cout << "Исходная матрица:\n";
+
     print_matrx(Mas3, 1);
+
+    cout << "\nВектор b:\n";
+
+    print_residuals(Mas2);
+
+
+    cout << "\n\nТочность: " << e << "\n" << "\nМаксимальное количество итераций: " << max_num_iter;
+
 
     if (!check_dia())
     {
-        cout << "На диагонали обнаружены нулевые элементы";
+        cout << "\n\n\n!!!На диагонали обнаружены нулевые элементы!!!";
         return 0;
     }
     int num_iter = 0;
-    /*
+
+
+    //Вычисление по методу Якоби
     num_iter = jacob(max_num_iter);
 
+    cout << "\n\n\nВычисление по методу Якоби:\n\n";
 
-    cout << "\n\nКоличество итераций: " << num_iter;
+    
     if (num_iter < 0)
     {
-        cout << "Решение расходится";
+        cout << "Решение расходится, т.к. количество итераций < 0";
     }
     else
     {
-        //вывод количества итераций
-        //вывод массива 
+        write_good(num_iter, Mas4_1, Mas5_1);
     }
-    */
 
+    //Вычисление по методу Зейделя
+    cout << "\n\n\nВычисление по методу Зейделя:\n\n";
 
     num_iter = Zeidel(max_num_iter);
-    cout << "\n\nКоличество итераций: " << num_iter;
-
-
-
+    if (num_iter < 0)
+    {
+        cout << "Решение расходится, т.к. количество итераций < 0";
+    }
+    else
+    {
+        write_good(num_iter, Mas4_2, Mas5_2);
+    }
+    
+    cout << "\n\nДля выхода нажмите любую клавишу\n\n";
+    _getch();
 }
